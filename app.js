0f9988513422f135c5b5117e2983f6a7649ed724
dinosantic <dinos.mos@gmail.com>
2025-03-23 19:59:28 +0100

    Added middleware for error handling.

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import router from "./routes/index.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use("/api/v1", router);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the app");
