03094562192503a57311d5ad66f211ff084e3aa2
dinosantic <dinos.mos@gmail.com>
2025-03-26 21:45:16 +0100

    Added arcjet api protection.

--- config/env.js
+++ config/env.js
@@ -7,5 +7,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 // Load the environment variables from the appropriate .env file
 config({ path: `.env.${process.env.NODE_ENV}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
